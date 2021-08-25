41d8e82479681e27a931f9d990a704af2ddeb3e0
Dave Gaeddert <dave.gaeddert@gmail.com>
2021-08-25 13:50:22 -0500

    Fix exports

--- cls_client/shared/exports.py
+++ cls_client/shared/exports.py
@@ -1,8 +1,8 @@
 from .settings import settings as _settings
-
-
 # These are the things we'll export for the client libraries
 # (nothing beyond these should be counted on)
+from .events import track_event, dispatch_events
+
 set_project_key = _settings.set_project_key
 set_project_slug = _settings.set_project_slug
 set_request_permission_prompt = _settings.set_request_permission_prompt
